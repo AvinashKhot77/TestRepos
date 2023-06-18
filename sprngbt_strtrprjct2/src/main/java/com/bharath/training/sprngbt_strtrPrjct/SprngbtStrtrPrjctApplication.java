d13fabbfd49ab3d523d3d35c6375033307e786a6
Avinash <avinashkhot51@gmail.com>
2023-06-18 14:48:27 +0530

    Test

---
+++ sprngbt_strtrprjct2/src/main/java/com/bharath/training/sprngbt_strtrPrjct/SprngbtStrtrPrjctApplication.java
@@ -0,0 +1,13 @@
+package com.bharath.training.sprngbt_strtrPrjct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngbtStrtrPrjctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngbtStrtrPrjctApplication.class, args);
+	}
+
+}
---
+++ sprngbt_strtrprjct2/src/test/java/com/bharath/training/sprngbt_strtrPrjct/SprngbtStrtrPrjctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.training.sprngbt_strtrPrjct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngbtStrtrPrjctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
